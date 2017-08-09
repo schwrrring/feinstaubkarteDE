15c8068e2c8589d7f461deac4200576bd882f06c
malte hildebrand <malte.hildebrand@gmail.com>
2017-08-09 12:14:50 +0200

    added leaflet mit mapbox

--- src/index.js
+++ src/index.js
@@ -18,4 +18,12 @@ export function update() {
 
 export function draw() {
   // The draw function is called when the template first loads
+				var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox.streets',
+            accessToken: 'pk.eyJ1IjoibHVuZGVsaXVzIiwiYSI6ImNpdWljbmV4eTAwM2Uyb21kczN6bndrb2kifQ.AXS9vjUNgfpx8zrAfNT2pw'
+        }).addTo(mymap);
 }
